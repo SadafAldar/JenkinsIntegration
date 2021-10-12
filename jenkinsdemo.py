f688b88bbab56e6ea81583ee8f002ec37bcab786
SadafAldar <89131811+SadafAldar@users.noreply.github.com>
2021-10-12 13:23:23 +0530

    Add files via upload

---
+++ jenkinsdemo.py
@@ -0,0 +1,10 @@
+print('My Details')
+print('------------------------------')
+print('''
+Name: Sadaf Aldar
+Class/Div: TE6
+Roll No: 03
+Department: IT
+Batch: A
+''')
+print('------------------------------')
